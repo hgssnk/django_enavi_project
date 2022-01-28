b41d9500d9da917d04edde3a5f89fa14ed9ee1a0
hgssnk <hgssnk@gmail.com>
2022-01-28 23:37:35 +0900

    雑多

--- accounts/static/js/login.js
+++ accounts/static/js/login.js
@@ -144,3 +144,18 @@
     }
 
 })(jQuery);
+
+function addSticky() {
+    $('.slide').each(function() {
+        var scrollerAnchor = $(this).offset().top;
+        if (window.scrollY >= scrollerAnchor) {
+            $(this).addClass('fix-it');
+        } else {
+            $(this).removeClass('fix-it');
+        }
+    });
+}
+
+$(window).scroll(function() {
+    addSticky();
+});
